6de0585b8461cfcb9ce14ef6b9cb8fc699d84022
Sean Smith <seanwssmith@gmail.com>
2016-03-11 17:50:03 -0800

    A profesor is one who talks in someone else's sleep.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 View and restore tabs that are open on devices.
 
-![In Action](example.png)
+![In Action](images/example.png)
 
 You must be using chrome and singed in on each device.
--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+// Sean Smith 2016
+// seanssmith.com
+// Feel free to copy!
+
 // Get all the devices that the user uses
 chrome.sessions.getDevices( function (devices){ 
 	
@@ -6,24 +10,26 @@ chrome.sessions.getDevices( function (devices){
 
 	// Get the devices
 	for (i = 0; i < devices.length; i++) {
-		$("#devices").append("<div class='device' data-sessionid='"+devices[i].sessions[0].window.sessionId+"'>"+devices[i].deviceName+"</div>");
 		var sessions = devices[i].sessions;
+		var device_name = devices[i].deviceName;
 
 		// For each session on a device
 		for (j = 0; j < sessions.length; j++) {
 			if (sessions[j].window) {
+				$("#devices").append("<div class='device' data-sessionid='"+sessions[j].window.sessionId+"'>"+device_name+"</div>");
 				var tabs = sessions[j].window.tabs;
 
 				// For each window on a device
 				for (k = 0; k < tabs.length; k++) {
-					// Insert a favicon if availible
-					$("#devices").append("<div class='link device-entry' data-url='"+tabs[k].url+"'><img class='link-img' src='chrome://favicon/size/16@1x/"+tabs[k].url+"'>"+tabs[k].title+"</div>");
+					// Add the tab to the list
+					$("#devices").append("<div class='link device-entry' data-sessionid='"+tabs[k].sessionId+"'><img class='link-img' src='chrome://favicon/size/16@1x/"+tabs[k].url+"'>"+tabs[k].title+"</div>");
 				}
 			}
 			else if (sessions[j].tab) {
-				alert("tab");
-				var favicon_url = sessions[j].tab.favIconUrl ? "<img src='"+sessions[j].tab.favIconUrl+"'>" : "";
-				$("#devices").append("<li class='link' data-url='"+sessions[j].tab.url+"'>"+favicon_url+"<a href='#'>"+sessions[j].tab.title+"</a></li>")
+				// Add device name
+				$("#devices").append("<div class='device' data-sessionid='"+sessions[j].tab.sessionId+"'>"+device_name+"</div>");
+				// List tab inline
+				$("#devices").append("<div class='link device-entry'><img class='link-img' src='chrome://favicon/size/16@1x/"+sessions[j].tab.url+"'>"+sessions[j].tab.title+"</div>");
 			}
 		}
 	}
@@ -35,12 +41,9 @@ function listen () {
 	// If a link is clicked open a new tab (unfocused) and remove link from list
 	// Also close the tab on the device
 	$(".link").click(function () {
-	  	var url = $(this).attr("data-url");
-	  	var tabid = $(this).attr("data-tabid");
+		var sessionid = $(this).attr("data-sessionid");
 	  	$(this).remove();
-		chrome.tabs.create({"url": url, "active": false}, function (tab){
-			console.log("url = "+url);
-		});
+	  	chrome.sessions.restore(sessionid);
 	});
 
 	// If a device is clicked, trigger a new window with all the tabs
